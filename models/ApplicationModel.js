38415d75379137eb42b62dc9de39efbc82e8fb43
Remma27 <emrodriguezso@est.utn.ac.cr>
2024-08-06 14:27:26 -0600

    chore: Update field name in ApplicationModel and userModel

--- models/ApplicationModel.js
+++ models/ApplicationModel.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const applicationSchema = new mongoose.Schema({
-    AplicationID: { type: Number, required: true },
+    AplicacionID: { type: Number, required: true },
     JobSeekerID: { type: Number, required: true },
     JobListingID: { type: Number, required: true },
     aplicationDate: { type: Date, required: true },
--- models/userModel.js
+++ models/userModel.js
@@ -14,7 +14,7 @@ const userSchema = new mongoose.Schema({
         type: Number,
         required: true
     },
-    cedula: {
+    Cedula: {
         type: Number,
         required: true
     },
